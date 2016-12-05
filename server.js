52fa52d6cb247ec4e5b54b970b34fb10e27346f7
Makk <makk.bit@outlook.com>
2016-12-06 03:43:35 +0530

    almost done

--- server.js
+++ server.js
@@ -1,8 +1,24 @@
-var express = require('express');
-var app = express();
+const express = require('express');
+const app = express();
+
+app.set('views', './views');
+app.set('view engine', 'pug');
 
 app.get('/', (req,res) =>  {
-	res.send('Hello World');
+	res.render('index');
+});
+
+app.get('/:str', (req,res) => {
+	// let time = new Date(req.params);
+	// console.log(decodeURIComponent(t=req.params));
+	// console.log(Date.parse(time));
+	// res.send("hello");
+	console.log(req.params.str);
+	var value = req.params.str;
+	console.log(new Date(value));
+	var date = new Date(value);
+	console.log(date.getTime());
+	
 });
 
 app.listen(8080, function(){
